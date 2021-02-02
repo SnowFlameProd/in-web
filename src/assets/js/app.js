bf722eeffad39d5cd9f3b7326023853be34de20e
SnowFlame <genylep@mail.ru>
2021-02-02 19:33:37 +0300

    сверстал тестовый сайт визитку без адаптива

---
+++ src/assets/js/app.js
@@ -0,0 +1,9 @@
+//= ../../../node_modules/jquery/dist/jquery.min.js
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+//= ../../../node_modules/slick-carousel/slick/slick.js
+
+$(function() {
+
+  //= components/slick.js
+
+})
